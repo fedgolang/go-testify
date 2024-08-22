a1e571b40c053d79e73d54d47c9e3b013712276f
Dmitry Fedotov <FedotovQA@yandex.ru>
2024-08-22 17:58:33 +0300

    sorted the tests in sequence as in the task

--- main_test.go
+++ main_test.go
@@ -12,25 +12,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-// Если в параметре count указано больше, чем есть всего, должны вернуться все доступные кафе
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	expectedRes := "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент"
-	totalCount := 4                                                                                                // Кол-во кафешек в moscow
-	req := httptest.NewRequest("GET", "/cafe?count="+fmt.Sprintf("%d", totalCount+rand.Int())+"&city=moscow", nil) // Выведем рандомное число больше известного кол-ва, так постараемся избежать парадокса пестицида
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
-	require.Equal(t, responseRecorder.Code, 200) // Проверяем, что при большем кол-ве всё равно вернём ОК, если !ОК валим тест
-
-	body := responseRecorder.Body.String() // Запишем тело в строку
-	list := strings.Split(body, ",")       // Создадим слайс значений со всеми кафешками
-
-	assert.Equal(t, len(list), totalCount) // проверим, что кол-во вернувшихся кафешек равно ожидаемому значению
-	assert.Equal(t, body, expectedRes)     // Для точности сверим, что все кафешки были выведены
-}
-
 // Запрос сформирован корректно, сервис возвращает код ответа 200 и тело ответа не пустое
 func TestMainHandlerHappyResult(t *testing.T) {
 	expectedCode := 200                                                 // Ожидаемый код
@@ -40,10 +21,11 @@ func TestMainHandlerHappyResult(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	require.Equal(t, responseRecorder.Code, expectedCode) // Сверим, что код ответа = expectedCode
+	require.Equal(t, responseRecorder.Code, expectedCode) // Сверим, что код ответа = expectedCode, если !ОК останавливаем тест
 	assert.NotEmpty(t, responseRecorder.Body.String())    // Проверим, что тело ответа не пустое
 }
 
+// Город, который передаётся в параметре city, не поддерживается. Сервис возвращает код ответа 400 и ошибку wrong city value в теле ответа.
 func TestMainHandlerWrongCity(t *testing.T) {
 	expectedCode := 400
 	expectedBody := "wrong city value"
@@ -53,6 +35,26 @@ func TestMainHandlerWrongCity(t *testing.T) {
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
-	require.Equal(t, responseRecorder.Code, expectedCode)         // Сверим, что код ответа = expectedCode
+	require.Equal(t, responseRecorder.Code, expectedCode)         // Сверим, что код ответа = expectedCode, если !ОК останавливаем тест
 	assert.Equal(t, responseRecorder.Body.String(), expectedBody) // сверим, что в тело вернулось ожидаемое значение expectedBody
+}
+
+// Если в параметре count указано больше, чем есть всего, должны вернуться все доступные кафе
+func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
+	expectedCode := 200
+	expectedRes := "Мир кофе,Сладкоежка,Кофе и завтраки,Сытый студент"
+	totalCount := 4                                                                                                // Кол-во кафешек в moscow
+	req := httptest.NewRequest("GET", "/cafe?count="+fmt.Sprintf("%d", totalCount+rand.Int())+"&city=moscow", nil) // Выведем рандомное число больше известного кол-ва, так постараемся избежать парадокса пестицида
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	require.Equal(t, responseRecorder.Code, expectedCode) // Проверяем, что при большем кол-ве всё равно вернём ОК, если !ОК останавливаем тест
+
+	body := responseRecorder.Body.String() // Запишем тело в строку
+	list := strings.Split(body, ",")       // Создадим слайс значений со всеми кафешками
+
+	assert.Equal(t, len(list), totalCount) // проверим, что кол-во вернувшихся кафешек равно ожидаемому значению
+	assert.Equal(t, body, expectedRes)     // Для точности сверим, что все кафешки были выведены
 }
